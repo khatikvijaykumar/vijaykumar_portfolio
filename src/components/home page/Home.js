cb666f032299f83e43c9db9eed8bf9ba6462c024
vijay kumar <vitik379@gmail.com>
2024-09-12 14:29:46 +0530

    yo yo

--- src/components/home page/Home.js
+++ src/components/home page/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
                 </div>
           </div>
           <div className=' mainbox  ' >
-            <img className='img' src="homepageimage.jpg" alt="image" />
+            <img className='img' src="/images/homepageimage.jpg" alt="img" />
           </div>
         </div> 
     </>
